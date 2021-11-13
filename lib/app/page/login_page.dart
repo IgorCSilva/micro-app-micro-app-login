19a3ac16a72e818e3028ab0bee397f4716296271
Igor Carneiro <igor.carneiro.silva13@gmail.com>
2021-11-13 18:35:44 -0300

    Alterando texto2.

--- lib/app/page/login_page.dart
+++ lib/app/page/login_page.dart
@@ -5,7 +5,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text('Login crieu'),
       ),
     );
   }
